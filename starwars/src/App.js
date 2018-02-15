b2607e66c3b9469d483e41e08f6f8968efd990da
Eric <erichech@gmail.com>
2018-02-15 16:59:40 -0500

    Finished setting it up for axios.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -12,12 +14,9 @@ class App extends Component {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
-    fetch('https://swapi.co/api/people')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        this.setState({ starwarsChars: data.results });
+    axios.get('https://swapi.co/api/people')
+      .then(response => {
+        this.setState({ starwarsChars: response.data.results });
       })
       .catch(err => {
         throw new Error(err);
